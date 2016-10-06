a3f1f6ded23c247e21c4d8afa7e1320c937c1add
loganborean <loganborean@gmail.com>
2016-10-06 14:00:05 -0700

    add stock lookup

--- MyApplication/app/src/main/java/com/example/logan/stockapp/Stock.java
+++ MyApplication/app/src/main/java/com/example/logan/stockapp/Stock.java
@@ -28,14 +28,13 @@ public class Stock extends AppCompatActivity {
 
         final Intent intent;
         final String stockSymbol;
-//        final TextView messageView;
-//
+
         intent = getIntent();
+
         stockSymbol = intent.getStringExtra("stockSymbol");
-//        messageView = (TextView) findViewById(R.id.stockDispaly);
-//        messageView.setText(stockSymbol);
         String url = "http://download.finance.yahoo.com/d/quotes.csv?s=" +
                 stockSymbol + "&f=nsl1t1cpoba&e=.csv";
+
         new RetrieveStockTask().execute(url);
 
 
@@ -46,6 +45,7 @@ public class Stock extends AppCompatActivity {
         protected String doInBackground(String... url) {
 
             URL oracle = null;
+            String dataLine = null;
             try {
                 oracle = new URL(url[0]);
             } catch (MalformedURLException e) {
@@ -61,8 +61,9 @@ public class Stock extends AppCompatActivity {
 
             String inputLine;
             try {
-                while ((inputLine = in.readLine()) != null)
-                    Log.d("~~~~~~~~~~~~~~~~~~~~~~~", inputLine);
+                while ((inputLine = in.readLine()) != null) {
+                    dataLine = inputLine;
+                }
             } catch (IOException e) {
                 Log.d("~~~~~~~~~~~~~~~~~~~~~~~", "2nd io ex");
             }
@@ -71,20 +72,76 @@ public class Stock extends AppCompatActivity {
             } catch (IOException e) {
                 Log.d("~~~~~~~~~~~~~~~~~~~~~~~", "close ex");
             }
-            return "hello";
+            return dataLine;
         }
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
 
-            final Intent intent;
-            final String stockSymbol;
-            final TextView messageView;
 
-            intent = getIntent();
-            stockSymbol = intent.getStringExtra("stockSymbol");
-            messageView = (TextView) findViewById(R.id.stockDispaly);
-            messageView.setText(stockSymbol);
+            String line = result;
+            String[] data;
 
+            String otherThanQuote = " [^\"] ";
+            String quotedString = String.format(" \" %s* \" ", otherThanQuote);
+            String regex = String.format("(?x) "+ // enable comments, ignore white spaces
+                            ",                         "+ // match a comma
+                            "(?=                       "+ // start positive look ahead
+                            "  (                       "+ //   start group 1
+                            "    %s*                   "+ //     match 'otherThanQuote' zero or more times
+                            "    %s                    "+ //     match 'quotedString'
+                            "  )*                      "+ //   end group 1 and repeat it zero or more times
+                            "  %s*                     "+ //   match 'otherThanQuote'
+                            "  $                       "+ // match the end of the string
+                            ")                         ", // stop positive look ahead
+                    otherThanQuote, quotedString, otherThanQuote);
+
+            data = line.split(regex, -1);
+
+            for(String s : data)
+                Log.d("~~~~~~~~", s);
+
+
+
+
+//            final Intent intent;
+//            final String stockSymbol;
+            TextView messageView;
+
+            messageView = (TextView) findViewById(R.id.stockName);
+            String text = "Stock Name: " + data[0];
+            messageView.setText(text);
+
+            messageView = (TextView) findViewById(R.id.symbol);
+            text = "symbol: " + data[1];
+            messageView.setText(text);
+
+            messageView = (TextView) findViewById(R.id.lastTrade);
+            text = "Last Trade: " + data[2];
+            messageView.setText(text);
+
+            messageView = (TextView) findViewById(R.id.tradeTime);
+            text = "Trade Time: " + data[3];
+            messageView.setText(text);
+
+            messageView = (TextView) findViewById(R.id.change);
+            text = "change: " + data[4];
+            messageView.setText(text);
+
+            messageView = (TextView) findViewById(R.id.prevClose);
+            text = "Previous Close: " + data[5];
+            messageView.setText(text);
+
+            messageView = (TextView) findViewById(R.id.open);
+            text = "Open: " + data[6];
+            messageView.setText(text);
+
+            messageView = (TextView) findViewById(R.id.bid);
+            text = "Bid: " + data[7];
+            messageView.setText(text);
+
+            messageView = (TextView) findViewById(R.id.ask);
+            text = "Ask: " + data[8];
+            messageView.setText(text);
         }
 
     }
